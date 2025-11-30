ddaa5d2f7a4efb3c47c1201c74d89a8375cfc33c
quietly-turning <quietly.turning@gmail.com>
2025-11-29 23:30:36 -0500

    hey guys
    good enough for now
    might return to embellish if there's time

--- art/pung/hey-guys/hey-guys.lua
+++ art/pung/hey-guys/hey-guys.lua
@@ -2,7 +2,11 @@ local af = Def.ActorFrame{}
 
 local walking = true
 local bpm = GAMESTATE:GetCurrentSong():GetDisplayBpms()[1]
-local sleep_duration = (60/bpm) * 0.5
+
+local walk_time = 2
+local step_sleep_duration  = (60/bpm) * 0.5
+
+local text_reveal_duration = 0.3
 
 af[#af+1] = LoadActor("grass.png")..{
   InitCommand=function(self)
@@ -20,18 +24,18 @@ af[#af+1] = LoadActor("./long-legged-fellow.png")..{
   InitCommand=function(self)
     local texture = self:GetTexture()
     local src_w   = texture:GetSourceWidth()
-    local zoom = 0.333
-    self:rotationy(180):zoom(zoom):valign(1):xy((src_w*0.333)*zoom, _screen.h - 30)
+    local zoom = 0.3
+    self:rotationy(180):zoom(zoom):valign(1):xy((src_w*zoom)*zoom, _screen.h - 30)
     self:queuecommand("StepLeft")
   end,
   StepLeftCommand=function(self)
     if walking then
-      self:sleep(sleep_duration):rotationz(6):queuecommand("StepRight")
+      self:sleep(step_sleep_duration):rotationz(6):queuecommand("StepRight")
     end
   end,
   StepRightCommand=function(self)
     if walking then
-      self:sleep(sleep_duration):rotationz(-6):queuecommand("StepLeft")
+      self:sleep(step_sleep_duration):rotationz(-6):queuecommand("StepLeft")
     end
   end,
   DoneWalkingCommand=function(self)
@@ -39,40 +43,115 @@ af[#af+1] = LoadActor("./long-legged-fellow.png")..{
   end
 }
 
-af[#af+1] = LoadActor("./speech-bubble 2x1.png")..{
-  Name="LLF-speech-bubble",
-  InitCommand=function(self) self:visible(false):zoom(0):animate(false):setstate(1) end,
-  DoneWalkingCommand=function(self) self:visible(true):sleep(0.3):bounceend(0.3):zoom(0.5) end
+-- a long-legged fellow's speech bubble AF
+af[#af+1] = Def.ActorFrame{
+  Name="LLF-speech-bubble-AF",
+  InitCommand=function(self) self:visible(false):zoom(0):xy(200,180) end,
+  DoneWalkingCommand=function(self) self:visible(true):sleep(0.3):bounceend(0.3):zoom(0.275) end,
+
+  LoadActor("./speech-bubbles 1x2.png")..{
+    Name="LLF-speech-bubble",
+    InitCommand=function(self) self:animate(false):setstate(0) end,
+  },
+  LoadActor("./dialogue 1x4.png")..{
+    Name="LLF-text",
+    InitCommand=function(self)
+      self:animate(false):setstate(1):cropright(1):y(-20)
+    end,
+    DoneWalkingCommand=function(self)
+      self:sleep(0.6):linear(text_reveal_duration):cropright(0):sleep(1.25):queuecommand("NextText")
+    end,
+    NextTextCommand=function(self)
+      self:cropright(1):setstate(2):sleep(0.1):linear(text_reveal_duration):cropright(0)
+    end,
+  }
 }
 
 
-local walk_time = 2
-local af2 = Def.ActorFrame{}
-af2.InitCommand=function(self) self:x(400) end
-af2.ShowCommand=function(self) self:linear(walk_time):x(-100):queuecommand("StopWalking") end
-af2.StopWalkingCommand=function(self)
-  walking = false
-  self:GetParent():queuecommand("DoneWalking")
-end
+-- a little stinker and a cat
+af[#af+1] = Def.ActorFrame{
+  InitCommand=function(self) self:x(400) end,
+  ShowCommand=function(self) self:linear(walk_time):x(-100):queuecommand("StopWalking") end,
+  StopWalkingCommand=function(self)
+    walking = false
+    self:GetParent():queuecommand("DoneWalking")
+  end,
 
-af2[#af2+1] = LoadActor("./im really good at drawing guys you just want to punch in the face - little stinkers if you will.png")..{
-  InitCommand=function(self)
-      self:zoom(0.35):valign(1):xy(_screen.w , _screen.h - 110)
-  end
+  -- a little stinker
+  LoadActor("./im really good at drawing guys you just want to punch in the face - little stinkers if you will.png")..{
+    InitCommand=function(self)
+        self:zoom(0.35):valign(1):xy(_screen.w , _screen.h - 110)
+    end
+  },
+
+  -- a cat
+  LoadActor("../../Lane/cat.png")..{
+    InitCommand=function(self)
+        self:zoom(0.275):valign(1):xy(_screen.w-150, _screen.h)
+    end
+  },
+
+  -- cat's speech bubble AF
+  Def.ActorFrame{
+    Name="cat-speech-bubble-AF",
+    InitCommand=function(self)
+      self:visible(false):zoom(0.35):xy(_screen.w-220,_screen.h-135)
+    end,
+    DoneWalkingCommand=function(self)
+      self:sleep(1.333):queuecommand("Reveal")
+    end,
+    RevealCommand=function(self) self:visible(true) end,
+
+    -- cat's speech-bubble
+    Def.Sprite{
+      Name="cat-speech-bubble",
+      OnCommand=function(self)
+        self:SetTexture( self:GetParent():GetParent():GetParent():GetChild("LLF-speech-bubble-AF"):GetChild("LLF-speech-bubble"):GetTexture() )
+        self:animate(false):setstate(1):rotationy(180)
+      end,
+    },
+
+    -- cat's text
+    Def.Sprite{
+      Name="cat-text",
+      OnCommand=function(self)
+        self:SetTexture(self:GetParent():GetParent():GetParent():GetChild("LLF-speech-bubble-AF"):GetChild("LLF-text"):GetTexture())
+        self:animate(false):setstate(0):cropright(1):xy(-10,-25):zoom(0.9)
+      end,
+      DoneWalkingCommand=function(self) self:sleep(0.8):linear(text_reveal_duration):cropright(0):sleep(1) end,
+    }
+  }
 }
 
-af2[#af2+1] = LoadActor("../../Lane/cat.png")..{
-  InitCommand=function(self)
-      self:zoom(0.275):valign(1):xy(_screen.w-150, _screen.h)
-  end
-}
 
-af[#af+1] = LoadActor("./speech-bubble 2x1.png")..{
-  Name="LLF-speech-bubble",
-  InitCommand=function(self) self:visible(false):zoom(0):animate(false):setstate(0):rotationy(180) end,
-  DoneWalkingCommand=function(self) self:visible(true):sleep(0.3):bounceend(0.3):zoom(0.5) end
-}
+-- ceiling peacock
+af[#af+1] = Def.ActorFrame{
+  InitCommand=function(self) self:visible(false):rotationz(180):xy(_screen.cx+100, -300):zoom(0.25) end,
+  DoneWalkingCommand=function(self) self:visible(true):sleep(2.5):decelerate(1):y(100) end,
 
-af[#af+1] = af2
+  LoadActor("../../Lane/peacock.png")..{
+    InitCommand=function(self) self:rotationy(180) end,
+  },
+
+  Def.ActorFrame{
+    InitCommand=function(self) self:xy(400, -400) end,
+
+    -- speech-bubble
+    Def.Sprite{
+      OnCommand=function(self)
+        self:SetTexture(self:GetParent():GetParent():GetParent():GetChild("LLF-speech-bubble-AF"):GetChild("LLF-speech-bubble"):GetTexture())
+        self:animate(false):setstate(0)
+      end,
+    },
+    -- text
+    Def.Sprite{
+      OnCommand=function(self)
+        self:SetTexture(self:GetParent():GetParent():GetParent():GetChild("LLF-speech-bubble-AF"):GetChild("LLF-text"):GetTexture())
+        self:animate(false):setstate(3):rotationz(-180):y(-10)
+      end,
+    }
+  }
+
+}
 
 return af
