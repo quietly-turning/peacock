ef2fb3ab3cbaffeeae57319bf6325c032c9322fa
quietly-turning <quietly.turning@gmail.com>
2025-11-30 17:41:15 -0500

    reduce textures needed for catsudawn animation
    on my machine, this speeds up overall stephart load time from ~9s to ~5s
    
    still not great(!), but a *noticeable* improvement

--- art/catsudawn/peacock/peacock.lua
+++ art/catsudawn/peacock/peacock.lua
@@ -1,27 +1,71 @@
 local WideScale = unpack(...)
+local bpm = GAMESTATE:GetCurrentSong():GetDisplayBpms()[1]
+local musicrate = 1/GAMESTATE:GetSongOptionsObject("ModsLevel_Song"):MusicRate()
 
 local af = Def.ActorFrame{}
+local texture
+local frame_dimension = 512
 
-for i=1,15 do
+
+-- frames 0→6 are used to decorate the left half of the screen
+-- frame 7 is the center piece
+-- frames 8→13 don't exist as assets, but once the for-loop reaches
+-- i > center_point, we can horizontally flip frames to decorate the
+-- right half of the screen
+local center_point = 7  -- last frame in the 4x2 set
+
+for i=0,13 do
   af[#af+1] = Def.ActorFrame{
     OnCommand=function(self)
-      self:glowshift()
-          :effectoffset((i%4)*(60/140))
-          :effectcolor1({0,0,0,0})
-          :effectcolor2({0,0,1,0.333})
+      self:diffuseshift()
+          :effectoffset(i*(60/bpm))
+          :effectclock("beat")
+          :effectcolor1(color("#4573B1"))
+          :effectcolor2(color("#E4FFCA"))
     end,
-    LoadActor(("./%d.png"):format(i))..{
-      InitCommand=function(self)
-        self:scaletocover(0,0,SCREEN_WIDTH, SCREEN_HEIGHT)
-      end,
+
+    Def.Sprite{
+      Condition=(i ~= center_point),  -- already handled by LoadActor() above
       OnCommand=function(self)
-        self:pulse():effectoffset((i%4)*(60/140))
-            :effectmagnitude(1,1.025,0)
-            :effectclock('beatnooffset')
+        self:SetTexture(texture)
+        self:animate(false):setstate(i % center_point):SetTextureFiltering(false)
+        self:y(_screen.cy)
+        self:zoomtoheight(_screen.h + 64)
+        self:zoomtowidth( frame_dimension * self:GetZoomedHeight()/frame_dimension )
+
+        self:halign( i >= center_point and 0 or 1 )
+        if (i >= center_point) then
+          self:rotationy(180):x(_screen.w-6)
+        else
+          self:x(_screen.cx+80) -- fudging some numbers to make the assets work while animated
+        end
+
+        self:pulse()
+        self:effectoffset(i*(60/bpm))
+        self:effectclock('beat')
+        self:effectmagnitude(1,1.025,0)
       end
     }
   }
 end
+
+-- middle piece of bg
+af[#af+1] = LoadActor("./background 4x2.png")..{
+  InitCommand=function(self)
+    texture = self:GetTexture()
+
+    self:animate(false):setstate(7):Center():SetTextureFiltering(true)
+    self:zoomtoheight(_screen.h)
+    self:zoomtowidth( frame_dimension * self:GetZoomedHeight()/frame_dimension )
+  end,
+  ShowCommand=function(self)
+    self:diffuseshift()
+    self:effectoffset(60/bpm)
+    self:effectclock("beat")
+    self:effectcolor1(color("#4573B1"))
+    self:effectcolor2(color("#8EC5CC"))
+  end
+}
 
 af[#af+1] = LoadActor("./peacock.png")..{
   InitCommand=function(self)
