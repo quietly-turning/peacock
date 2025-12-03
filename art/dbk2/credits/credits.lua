e120dd718183e6df5f75ece3f51712507fdf6111
quietly-turning <quietly.turning@gmail.com>
2025-12-03 00:26:57 -0500

    wip: credits

--- art/dbk2/credits/credits.lua
+++ art/dbk2/credits/credits.lua
@@ -22,10 +22,11 @@ local artists = {
 }
 
 local focus = { PlayerNumber_P1=0, PlayerNumber_P2=1 }
-local texture, af_ref
+local texture, af2_ref
 local base_path = GAMESTATE:GetCurrentSong():GetSongDir()
-local num_cols = 4
-local padding = 24
+local num_cols = 3
+
+local OtherPlayer = { PlayerNumber_P1="PlayerNumber_P2", PlayerNumber_P2="PlayerNumber_P1" }
 
 local InputActions = {
    -- decrement by 1, wrap to end if needed
@@ -67,70 +68,126 @@ local function InputHandler(event)
   if event.type ~= "InputEventType_FirstPress" then return end
   if not InputActions[event.button]            then return end
 
-  af_ref:playcommand("LoseFocus")
+  -- cause all artist names to lose focus
+  af2_ref:playcommand("LoseFocus")
+
+  -- update cursor index for player that generated input event
   InputActions[event.button](event.PlayerNumber)
 
-  local childName = ("Artist%d"):format(focus[event.PlayerNumber])
-  local cmdName   = ("%sFocus"):format(ToEnumShortString(event.PlayerNumber))
-  af_ref:GetChild(childName):playcommand(cmdName)
-end
+  -- -------------------------------------------------------
+  -- generate strings to find appropriate children to update
+  -- complex enough I broke this out into discrete lines rather than inlining it all
+  local childText   = ("Artist%d"):format(focus[event.PlayerNumber])
+  local cursorName  = ("%sCursor"):format(ToEnumShortString(event.PlayerNumber))
+  -- update text and cursor for player with input event
+  af2_ref:GetChild(childText):playcommand("Focus")
+  af2_ref:GetChild(cursorName):playcommand("ChangeFocus")
 
+  -- -------------------------------------------------------
+  -- update text and cursor for other player
+  local otherText   = ("Artist%d"):format(focus[OtherPlayer[event.PlayerNumber]])
+  local otherCursor = ("%sCursor"):format(ToEnumShortString(OtherPlayer[event.PlayerNumber]))
+  af2_ref:GetChild(otherText):playcommand("Focus")
+  af2_ref:GetChild(otherCursor):playcommand("ChangeFocus")
+  -- -------------------------------------------------------
+
+end
 
 -- ------------------------------------------------------
+local WideScale, IsEditMode, GetPlayerAF, GenerateSprite = unpack(LoadActor("../../../FGCHANGES/helpers.lua"))
+-- ------------------------------------------------------
 
-local text_zoom = 0.333
+local text_zoom = 0.3
 
 local af = Def.ActorFrame{}
-af.InitCommand=function(self)
-  af_ref = self
-  self:xy(200,130)
-end
 af.OnCommand=function(self)
-  self:GetChild("Artist0"):playcommand("P1Focus")
-  self:GetChild("Artist1"):playcommand("P2Focus")
+  af2_ref:GetChild("Artist0"):playcommand("P1Focus")
+  af2_ref:GetChild("Artist1"):playcommand("P2Focus")
 end
 af.ShowCommand=function(self)
   SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
+
+  for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+    GAMESTATE:GetPlayerState(player):GetPlayerOptions("ModsLevel_Song"):Dark(1,2, true):Stealth(1,2)
+  end
 end
 
 af[#af+1] = LoadActor("./thanks.png")..{
-  InitCommand=function(self) self:xy(_screen.cx-200, -60):zoom(0.5) end
+  InitCommand=function(self)
+    self:valign(0):xy(_screen.cx, -10):zoom(0.5)
+  end
 }
 
-af[#af+1] = LoadActor("./artists 2x10")..{
+local af2 = Def.ActorFrame{}
+af2.Name="ArtistsAF"
+af2.InitCommand=function(self)
+  af2_ref = self
+  self:xy(_screen.w*0.25, _screen.h*0.25)
+end
+
+af2[#af2+1] = Def.Sprite{
+  Name="P1Cursor",
+  OnCommand=function(self)
+    self:SetTexture(texture):animate(false):setstate(20)
+    self:zoomx(text_zoom * 1.25):zoomy(text_zoom * 1.05)
+    self:diffuse(0.4,0.4,1,1)
+  end,
+  ChangeFocusCommand=function(self)
+    local p1_focus = self:GetParent():GetChild(("Artist%d"):format(focus.PlayerNumber_P1))
+    self:xy(p1_focus:GetX()-20, p1_focus:GetY())
+  end
+}
+
+af2[#af2+1] = Def.Sprite{
+  Name="P2Cursor",
+  OnCommand=function(self)
+    self:SetTexture(texture):animate(false):setstate(21)
+    self:zoomx(text_zoom * 1.25):zoomy(text_zoom * 1.05)
+    self:diffuse(0.5,1,0.5,0.9)
+  end,
+  ChangeFocusCommand=function(self)
+    local p2_focus = self:GetParent():GetChild(("Artist%d"):format(focus.PlayerNumber_P2))
+    self:xy(p2_focus:GetX()+20, p2_focus:GetY())
+  end
+}
+
+-- 1st artist in the list, used to load the texture of all artist names
+af2[#af2+1] = LoadActor("./artists 2x11")..{
   Name="Artist0",
   InitCommand=function(self)
     self:animate(false):setstate(0):xy(0, 0):zoom(text_zoom)
     texture = self:GetTexture()
   end,
-  P1FocusCommand=function(self)
-    self:diffuse(1,0,0,1)
-  end,
-  P2FocusCommand=function(self)
-    self:diffuse(0,0,1,1)
+  FocusCommand=function(self)
+    self:diffuse(0,0,0,1)
   end,
   LoseFocusCommand=function(self)
     self:diffuse(1,1,1,1)
   end
 }
 
+local col_width = 190
+local row_height = 30
+
+-- all other artists added in for-loop, re-using the already-loaded texture from 1st artist
 for i=1, #artists-1 do
-  af[#af+1] = Def.Sprite{
+  af2[#af2+1] = Def.Sprite{
     Name=("Artist%d"):format(i),
     InitCommand=function(self)
       self:SetTexture(texture):animate(false):setstate(i):zoom(text_zoom)
-      self:xy((i%num_cols)*164, (math.floor(i/num_cols))*30 )
+      self:xy((i%num_cols) * col_width, (math.floor(i/num_cols)) * row_height)
     end,
-    P1FocusCommand=function(self)
-      self:diffuse(1,0,0,1)
-    end,
-    P2FocusCommand=function(self)
-      self:diffuse(0,0,1,1)
+    FocusCommand=function(self)
+      self:diffuse(0,0,0,1)
     end,
     LoseFocusCommand=function(self)
       self:diffuse(1,1,1,1)
     end
   }
 end
+
+af[#af+1] = af2
+
+
 
 return af
